8b298717f84418986768ced52eb23d678776ceb2
Fernando11000 <fernandoferreira10@hotmail.com.br>
2023-05-28 17:47:28 -0300

    refatora e adiciona funcionalidades

---
+++ src/components/FundoBarbeariaComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="fit">
+    
+    <q-img class="" src="../assets/tabelaPrecos.jpeg" 
+                ></q-img>    
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+
+</style>
+
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,49 +1,55 @@
-<template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+<template classs="">
+  <q-page class="flex flex-center bg-blue-4">
+    <div class="q-pa-md row justify-center">
+    <FundoBarbeariaComponent class="mobile-only" title="Example component" active></FundoBarbeariaComponent>
+    <div style="width: 100%; max-width: 400px">
+      <q-chat-message v-show="bemVindo" avatar="../assets/pablo.png" name="Barbearia Tem Tudo" :text="['Olá, Bem vindo!']"
+        stamp="agora mesmo" bg-color="grey-12" />
+
+      <q-chat-message v-show="!loading" avatar="../assets/pablo.png" name="Barbearia Tem Tudo" stamp="agora mesmo"
+        bg-color="grey-12">
+        <div>
+          <span>
+            Infelizmente esse site ainda não
+          </span>
+          <q-space />
+          <span> tem uma versão para </span>
+          <q-space />
+          <span>computadores
+          </span>
+          <span> &nbsp;😞</span>
+        </div>
+      </q-chat-message>
+
+      <q-chat-message v-show="loading" name="Barbearia Tem Tudo" avatar="../assets/pablo.png" bg-color="grey-12">
+        <q-spinner-dots size="2rem" />
+      </q-chat-message>
+
+    </div>
+    </div>
   </q-page>
 </template>
 
-<script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { defineComponent, ref } from 'vue';
+<script lang="ts" setup >
+import FundoBarbeariaComponent from 'src/components/FundoBarbeariaComponent.vue';
+import { ref, onMounted } from 'vue'
+const loading = ref(true)
+const bemVindo = ref(false)
 
-export default defineComponent({
-  name: 'IndexPage',
-  components: { ExampleComponent },
-  setup() {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
-    return { todos, meta };
-  }
-});
+const ordemMensagens = () => {
+  console.log('Executou')
+  setTimeout(() => {
+    bemVindo.value = true
+  }, 1000);
+  setTimeout(() => {
+    loading.value = false
+  }, 3000);
+}
+onMounted(() => {
+  ordemMensagens()
+
+})
+
+
 </script>
+
