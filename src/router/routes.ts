464ff6f2dfd8602a85a667470d39b93db652e5cd
Fernando11000 <fernandoferreira10@hotmail.com.br>
2023-05-28 22:33:10 -0300

    para publicacao no firebase

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,7 +2,7 @@ import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   { //lembrar que coloquei para qualquer rota cair no index
-    path: '/',
+    path: '/barbearia',
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
