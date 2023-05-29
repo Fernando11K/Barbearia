86c3dbd8131f0636529d5183d4f88ba41f8d70ae
Fernando11000 <fernandoferreira10@hotmail.com.br>
2023-05-28 22:01:56 -0300

    descomenta rota

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,18 +2,16 @@ import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   { //lembrar que coloquei para qualquer rota cair no index
-    path: '/barbearia',
+    path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
-/*  {
+{
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
-  },
-  */
+  }
+
 ];
 
 export default routes;
