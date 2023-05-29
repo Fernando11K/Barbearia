938d7d6becdd9dd9e595c89a60314abf544ac664
Fernando11000 <fernandoferreira10@hotmail.com.br>
2023-05-28 22:28:08 -0300

    altera configurações do firebase

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,11 +7,13 @@ const routes: RouteRecordRaw[] = [
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
 
-{
+  // Always leave this as last one,
+  // but you can also remove it
+/*  {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
-  }
-
+  },
+  */
 ];
 
 export default routes;
