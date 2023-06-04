59e606d21b31121521b53f33f9e43f18ce9951fa
Fernando11000 <fernandoferreira10@hotmail.com.br>
2023-06-04 19:13:07 -0300

    adiciona funcao para mudanca da cor do sepator dinamicamente

--- src/components/MainLayout/QFooterComponent.vue
+++ src/components/MainLayout/QFooterComponent.vue
@@ -1,22 +1,24 @@
-<template>   
-   <q-footer reveal elevated class="bg-blue-8 glossy text-white ">
-      <q-separator :color="corAtual" class="shadow-up-24" />
-      <q-toolbar class="mobile-only ">
-        <div class="row justify-around col-12 fonte-footer ">
-          <q-btn size="md" flat round icon="fa-solid fa-bars" class="fonte-footer  " @click="capturaClickDoBotaoPainelLateralEsquerdo">
-          </q-btn>
-          <q-btn size="md" flat round icon="fa-solid fa-house " class="fonte-footer disabled " @click="funcionalidadeNaoDisponivel">
-          </q-btn>
-          <q-btn size="md" flat round icon="fa-solid fa-bell fa-beat" class="fonte-footer disabled " @click="funcionalidadeNaoDisponivel">
-          </q-btn>
-          <q-btn size="md" flat round class="fonte-footer " @click="abrirWhatsApp">
-            <q-icon size="md" name="fa-brands fa-whatsapp fa-bounce" color="light-green-13">
-            </q-icon>
-          </q-btn>
-
-        </div>
-      </q-toolbar>
-    </q-footer>        
+<template>
+  <q-footer reveal elevated class="bg-blue-8 glossy text-white ">
+    <q-separator :color="corAtual" class="shadow-up-24 glossy " />
+    <q-toolbar class="mobile-only ">
+      <div class="row justify-around col-12 fonte-footer ">
+        <q-btn size="md" flat round icon="fa-solid fa-bars" class="fonte-footer  "
+        @click="capturaClickDoBotaoPainelLateralEsquerdo">
+        </q-btn>
+        <q-btn size="md" flat round icon="fa-solid fa-house " class="fonte-footer disabled "
+          @click="funcionalidadeNaoDisponivel">
+        </q-btn>
+        <q-btn size="md" flat round icon="fa-solid fa-bell fa-beat" class="fonte-footer disabled "
+          @click="funcionalidadeNaoDisponivel">
+        </q-btn>
+        <q-btn size="md" flat round class="fonte-footer " @click="abrirWhatsApp">
+          <q-icon size="md" name="fa-brands fa-whatsapp fa-bounce" color="light-green-13">
+          </q-icon>
+        </q-btn>
+      </div>
+    </q-toolbar>
+  </q-footer>
 </template>
 
 <script setup lang="ts">
@@ -24,9 +26,14 @@ import { ref, onMounted } from 'vue'
 const emits = defineEmits(['clickBotaoPainelLateralEsquerdo']);
 import alert from '../../hooks/alert'
 const alerta = alert()
-const corAtual = ref('')
-const cores = ['red-1', 'red-2', 'red-3', 'red-4'];
-const trocarCor = () => cores[0]
+const corAtual = ref<string>()
+const corAtualIndex = ref<number>(0)
+const cores = ['blue-3', 'blue-4', 'blue-5', 'blue-6' ];
+const trocarCor = () => {
+  corAtualIndex.value = (corAtualIndex.value + 1) % cores.length;
+  corAtual.value = cores[corAtualIndex.value]
+}
+onMounted(() => setInterval(trocarCor, 500));
 
 
 
@@ -36,9 +43,8 @@ const message = ref('Olá! Está funcionando?');
 const capturaClickDoBotaoPainelLateralEsquerdo = () => emits('clickBotaoPainelLateralEsquerdo')
 
 const abrirWhatsApp = () => { const url = `https://api.whatsapp.com/send?phone=${celular.value}&text=${encodeURIComponent(message.value)}`; window.open(url, '_blank') }
-const funcionalidadeNaoDisponivel = () =>  alerta.warning('A funcionalidade estará disponível em breve!')
- 
-onMounted(() => setInterval(trocarCor, 1000));
+const funcionalidadeNaoDisponivel = () => alerta.warning('A funcionalidade estará disponível em breve!')
+
 
 
 </script>
