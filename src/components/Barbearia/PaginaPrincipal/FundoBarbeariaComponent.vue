929b4114275ec8fb92e36577df8e7e681bcc1bfd
unknown <unknown@unknown.unknown>
2023-07-23 23:13:11 -0300

    conserta posicao chat

--- src/components/Barbearia/PaginaPrincipal/FundoBarbeariaComponent.vue
+++ src/components/Barbearia/PaginaPrincipal/FundoBarbeariaComponent.vue
@@ -1,7 +1,10 @@
-<template>  
-    <q-img class="relative-position" width="100%" height="100%" :src="require('../../../assets/tabelaPrecos.jpeg')" /> 
-    <q-img class="relative-position" width="100%" height="100%" :src="require('../../../assets/tabelaPrecos.jpeg')" /> 
-    <CarrosselComponent v-if="false" class="abosolute-top-center full-width " />      
+<template>
+<div>
+
+  <q-img class="relative-position" width="100%" height="100%" :src="require('../../../assets/tabelaPrecos.jpeg')" /> 
+  <q-img class="relative-position" width="100%" height="100%" :src="require('../../../assets/tabelaPrecos.jpeg')" /> 
+  <CarrosselComponent v-if="false" class="abosolute-top-center full-width " />      
+</div>  
 </template>
 
 <script lang="ts" setup>
