2ac81cf9ea0e89f53a69c3737dc16fb2b4afa246
unknown <unknown@unknown.unknown>
2023-07-24 22:27:37 -0300

    fix

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,9 +3,9 @@
       <FundoBarbeariaComponent class="mobile-only" />
     <div class="q-pa-md row justify-center desktop-only">
       <div style="width: 100%; max-width: 400px" class="">
-        <q-chat-message v-show="bemVindo" avatar="../assets/pablo.png" name="Barbearia"
+        <q-chat-message v-show="bemVindo" avatar="https://cdn.pixabay.com/photo/2018/01/09/14/23/vector-3071686_1280.png" name="Barbearia"
         :text="['Olá, Bem vindo!']" stamp="agora mesmo" bg-color="grey-12" class="" />
-        <q-chat-message v-show="!loading" avatar="../assets/pablo.png" name="Barbearia" stamp="agora mesmo"
+        <q-chat-message v-show="!loading" avatar="https://cdn.pixabay.com/photo/2018/01/09/14/23/vector-3071686_1280.png" name="Barbearia" stamp="agora mesmo"
         bg-color="grey-12">
         <div>
             <span>
@@ -20,7 +20,7 @@
           </div>
         </q-chat-message>
 
-        <q-chat-message v-show="loading" name="Barbearia" avatar="../assets/pablo.png" bg-color="grey-12"
+        <q-chat-message v-show="loading" name="Barbearia" avatar="https://cdn.pixabay.com/photo/2018/01/09/14/23/vector-3071686_1280.png" bg-color="grey-12"
           class="">
           <q-spinner-dots  />
         </q-chat-message>
