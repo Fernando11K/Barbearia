74e746cff61446f82784bb8ba5f047007fd9f6ce
unknown <unknown@unknown.unknown>
2023-07-24 22:07:56 -0300

    fix

--- src/components/Barbearia/PaginaPrincipal/FundoBarbeariaComponent.vue
+++ src/components/Barbearia/PaginaPrincipal/FundoBarbeariaComponent.vue
@@ -1,7 +1,9 @@
 <template>
-  <q-img class="relative-position" width="100%" height="100%" :src="require('../../../assets/tabelaPrecos.jpeg')" /> 
-  <q-img class="relative-position" width="100%" height="100%" :src="require('../../../assets/tabelaPrecos.jpeg')" /> 
-  <CarrosselComponent v-if="false" class="abosolute-top-center full-width " />      
+  <div>
+    <q-img class="relative-position" width="100%" height="100%" :src="require('../../../assets/tabelaPrecos.jpeg')" /> 
+    <q-img class="relative-position" width="100%" height="100%" :src="require('../../../assets/tabelaPrecos.jpeg')" /> 
+    <CarrosselComponent v-if="false" class="absolute-top-center full-width " />      
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -10,7 +12,7 @@ import CarrosselComponent from './CarrosselComponent.vue'
 </script>
 
 <style lang="scss" scoped>
-.abosolute-top-center {
+.absolute-top-center {
   position: absolute;
   top: 1px;
   
