58a6a2ea070323954ba96b95f4b7aa77fd9addd0
Fernando11K <fernandoferreira10@hotmail.com.br>
2024-01-07 15:23:54 -0300

    Hotfix: Resolve problemas no InputDate

--- src/components/Agendamento/InputDate.vue
+++ src/components/Agendamento/InputDate.vue
@@ -4,7 +4,8 @@
     <template v-slot:prepend>
       <q-icon color="primary" name="event" class="cursor-pointer">
         <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-          <q-date today-btn :modelValue="modelValue" @update:modelValue="atualiza" mask="DD/MM/YYYY HH:mm">
+          <q-date today-btn :modelValue="modelValue" @update:modelValue="atualiza" mask="DD/MM/YYYY HH:mm"
+            :options="opcoesData">
             <div class="row items-center justify-end">
               <q-btn v-close-popup label="Fechar" color="primary" flat />
             </div>
@@ -17,7 +18,7 @@
       <q-icon name="access_time" class="cursor-pointer">
         <q-popup-proxy cover transition-show="scale" transition-hide="scale">
           <q-time id="horário agendamento" :modelValue="modelValue" @update:modelValue="atualiza" mask="DD/MM/YYYY HH:mm"
-            format24h>
+            format24h :hour-options="[9, 10, 11, 12, 13, 14, 15, 16, 17]" :minute-options="[0, 15, 30, 45]">
             <div class="row items-center justify-end">
               <q-btn v-close-popup label="Fechar" color="primary" flat />
             </div>
@@ -30,6 +31,7 @@
   
 <script lang="ts" setup>
 import { QInput } from 'quasar';
+import { defineProps } from 'vue'
 import { date } from 'quasar'
 const dataAtual = new Date();
 const data6MesFuturos = date.addToDate(dataAtual, { months: 6 })
@@ -48,6 +50,7 @@ const exibeMensagemValidacao = () => {
 const opcoesData = (data: string) => {
   return date.isBetweenDates(new Date(data.split('/')), dataAtual, data6MesFuturos, { onlyDate: true, inclusiveFrom: true, inclusiveTo: true });
 }
+
 
 
 </script>
