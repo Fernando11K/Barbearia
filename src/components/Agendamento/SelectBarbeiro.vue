4d4db9747aa5df63dd326cd6fb71f8c99499a4d7
Fernando11K <fernandoferreira10@hotmail.com.br>
2024-01-20 21:33:47 -0300

    remove propriedade não necessária SelectBarbeiro

--- src/components/Agendamento/SelectBarbeiro.vue
+++ src/components/Agendamento/SelectBarbeiro.vue
@@ -3,7 +3,7 @@
         transition-show="scale" transition-hide="scale" options-cover label="Selecione o profissional:" dense rounded
         outlined class="col-12 q-pr-xs"
         :rules="[val => val && val?.label?.length > 0 || 'Favor selecionar um profissional para o atendimento']"
-        behavior="menu" autofocus clearable :use-chips="!!modelValue?.label">
+        behavior="menu" autofocus :use-chips="!!modelValue?.label">
         <template v-slot:prepend>
             <q-icon name="fa-solid fa-user-tie" color="primary" />
         </template>
