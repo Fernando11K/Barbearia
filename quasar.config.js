3fb04ef8fb8714237553bc2dc1dea3fe7d86859c
Fernando11K <fernandoferreira10@hotmail.com.br>
2024-06-29 17:14:38 -0300

    refactor: move configuração do manifest.json para arquivo separado

--- quasar.config.js
+++ quasar.config.js
@@ -158,42 +158,7 @@ module.exports = configure(function (ctx) {
       // if using workbox in InjectManifest mode
       // chainWebpackCustomSW (/* chain */) {},
 
-      manifest: {
-        name: 'The Barber\'s Den Barbershop',
-        short_name: 'Barbearia do Den',
-        description: '',
-        display: 'standalone',
-        orientation: 'portrait',
-        background_color: '#ffffff',
-        theme_color: '#027be3',
-        icons: [
-          {
-            src: 'icons/icon-128x128.png',
-            sizes: '128x128',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-256x256.png',
-            sizes: '256x256',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-384x384.png',
-            sizes: '384x384',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
+      manifestFilename: 'manifest.json',
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/developing-cordova-apps/configuring-cordova
